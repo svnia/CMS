a8ac34b96eb19fe51d6e74f856e091bd30de12e1
svnia <100943529+svnia@users.noreply.github.com>
2023-05-10 01:11:04 +0200

    Update User.Class.php

--- src/User.Class.php
+++ src/User.Class.php
@@ -12,4 +12,25 @@ class User {
 
     }
 }
+    //gettery
+    public function getId() : int {
+        return $this->id;
+    }
+    public function getName() : string {
+        return $this->email;
+    }
+    public static function isAuth() : bool {
+
+        //sprawdz czy w sesji jest coś pod nazwą user
+        if(isset($_SESSION['user'])) {
+            //sprawdz czy to coś jest instacją klasy User
+            if($_SESSION['user'] instanceof User) {
+                //użytkownik zalogowany
+                return true;
+            }
+        }
+        //użytkownik niezalogowany
+        return false;
+    }
+    
 ?>
