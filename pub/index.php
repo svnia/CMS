03d94eb36757af7d537dbf859e1ad2eb575e1736
svnia <100943529+svnia@users.noreply.github.com>
2023-05-10 00:47:57 +0200

    Create index.php

---
+++ pub/index.php
@@ -0,0 +1,119 @@
+<?php
+require("./../src/config.php");
+session_start();
+use Steampixel\Route;
+
+Route::add('/', function() {
+    //strona wyświetlająca obrazki
+    global $twig;
+    //pobierz 10 najnowszych postów
+    $postArray = Post::getPage();
+    $twigData = array("postArray" => $postArray,
+                        "pageTitle" => "Strona główna",
+                        );
+    //jeśli użytkownik jest zalogowany to przekaż go do twiga
+    if(isset($_SESSION['user']))
+        $twigData['user'] = $_SESSION['user'];
+    $twig->display("index.html.twig", $twigData);
+});
+
+Route::add('/upload', function() {
+    //strona z formularzem do wgrywania obrazków
+    global $twig;
+    $twigData = array("pageTitle" => "Wgraj mema");
+    //jeśli użytkownik jest zalogowany to przekaż go do twiga
+    if(User::isAuth()) {
+        $twigData['user'] = $_SESSION['user'];
+        $twig->display("upload.html.twig", $twigData);
+    }
+    else {
+        //zwróć kod 403 czyli zabronione
+        http_response_code(403);
+    }    
+});
+
+Route::add('/upload', function() {
+    //wywoła się tylko po otrzymaniu danych metodą post na ten url
+    // (po wypełnieniu formularza)
+    global $twig;
+    if(isset($_POST['submit']))  {
+        Post::upload($_FILES['uploadedFile']['tmp_name'], $_POST['title'], $_POST['userId']);
+    }
+    //TODO: zmienić na ścieżkę względną
+    header("Location: http://localhost/sadistic/pub");
+}, 'post');
+
+Route::add('/register', function() {
+    global $twig;
+    $twigData = array("pageTitle" => "Zarejestruj użytkownika");
+    $twig->display("register.html.twig", $twigData);
+});
+
+Route::add('/register', function(){
+    global $twig;
+    if(isset($_POST['submit'])) {
+        User::register($_POST['email'], $_POST['password']);
+        header("Location: http://localhost/sadistic/pub");
+    }
+}, 'post');
+
+Route::add('/login', function(){
+    global $twig;
+    $twigData = array("pageTitle" => "Zaloguj użytkownika");
+    $twig->display("login.html.twig", $twigData);
+});
+
+Route::add('/login', function() {
+    global $twig;
+    if(isset($_POST['submit'])) {
+        if(User::login($_POST['email'], $_POST['password'])) {
+            //jeśli zalogowano poprawnie to wyświetl główną stronę
+            header("Location: http://localhost/sadistic/pub");
+        } else {
+            //jeśli nie zalogowano poprawnie wyświetl ponownie stronę logowania z komunikatem
+            $twigData = array("pageTitle" => "Zaloguj użytkownika",
+                                "message" => "Niepoprawny użytkownik lub hasło");
+            $twig->display("login.html.twig", $twigData);
+        }
+    }
+    
+
+}, 'post');
+
+Route::add('/admin', function() {
+    global $twig;
+    if(User::isAuth()) {
+        $postsList = Post::getPage(1, 100);
+        $twigData = array("postList" => $postsList);
+        $twig->display("admin.html.twig", $twigData);
+    } else {
+        http_response_code(403);
+    }
+});
+
+Route::add('/like/([0-9]*)', function($post_id) {
+    if(!User::isAuth()) {
+        http_response_code(403);
+    }
+    $user_id = $_SESSION['user']->getId();
+    $like = new Likes($post_id, $user_id, 1);
+    header("Location: /sadistic/pub");
+});
+
+Route::add('/dislike/([0-9]*)', function($post_id) {
+    if(!User::isAuth()) {
+        http_response_code(403);
+    }
+    $user_id = $_SESSION['user']->getId();
+    $like = new Likes($post_id, $user_id, -1);
+    header("Location: /sadistic/pub");
+});
+
+
+
+
+
+Route::run('/sadistic/pub');
+
+
+?>
