a900b5eb81c6dc3ad34f3fb62492535fecb2decc
svnia <100943529+svnia@users.noreply.github.com>
2023-05-10 01:09:09 +0200

    TWIG
    Według PHP - nie działający loader.

--- src/config.php
+++ src/config.php
@@ -8,4 +8,9 @@ $db = new mysqli("localhost", "root", "", "cms_js");
 require('Post.class.php');
 require('User.class.php');
 require('Likes.class.php');
+
+//loader to taki pomocnik do ładowania szablonów
+$loader = new Twig\Loader\FilesystemLoader('./../src/templates');
+//inicjujemy twiga
+$twig = new Twig\Environment($loader);
 ?>
