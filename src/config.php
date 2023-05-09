044298ecb88fb7048f1bf591a9bd099317672c5b
svnia <100943529+svnia@users.noreply.github.com>
2023-05-10 01:08:25 +0200

    Update config.php

---
+++ src/config.php
@@ -0,0 +1,11 @@
+<?php
+
+require("./../vendor/autoload.php");
+
+$db = new mysqli("localhost", "root", "", "cms_js");
+
+
+require('Post.class.php');
+require('User.class.php');
+require('Likes.class.php');
+?>
